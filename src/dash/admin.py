983562605995fa3f005e2c6c261f600d08c3fa55
saposki <saposki@gmail.com>
2016-02-18 22:20:15 -0800

    implementing csv upload

--- src/dash/admin.py
+++ src/dash/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from .models import Dash
 
-admin.site.register(Dash)
+class DashModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'timestamp']
+    search_fields = ['title']
+    class Meta:
+        model = Dash
+
+admin.site.register(Dash, DashModelAdmin)
--- src/dash/views.py
+++ src/dash/views.py
@@ -14,3 +14,33 @@ def dash_csv(request):
         'file': file
     }
     return render(request, 'dash_form.html', context)
+
+def dash_detail(request, id=None): #retrieve
+	data_instance = get_object_or_404(Dash, id=id)
+	context = {
+		"title": data_instance.title,
+		"data_instance": data_instance,
+	}
+	return render(request, 'dash_detail.html', context)
+
+def dash_list(request): #list items
+	queryset_list = Dash.objects.all().order_by('-timestamp')
+	paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+	page_request_var = 'page'
+
+	page = request.GET.get(page_request_var)
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+	    # If page is not an integer, deliver first page.
+		queryset = paginator.page(1)
+	except EmptyPage:
+	    # If page is out of range (e.g. 9999), deliver last page of results.
+		queryset = paginator.page(paginator.num_pages)
+
+	context = {
+		"object_list": queryset,
+		"title": "Recent Data",
+		'page_request_var': page_request_var
+	}
+	return render(request, 'dash_list.html', context)
