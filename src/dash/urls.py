71d57232b6d12755260e15c830c6dec41d5a62cd
saposki <saposki@gmail.com>
2016-03-02 19:55:03 -0800

    updated dash to query data set

--- src/dash/urls.py
+++ src/dash/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from .views import (
 	dash_list,
 	dash_create,
-	dash_detail,
+	# dash_detail,
 	dash_chart,
 	# post_update,
 	# post_delete,
@@ -14,7 +14,7 @@ urlpatterns = [
 	url(r'^$', dash_list, name='list'),
     url(r'^create/$', dash_create),
     url(r'^chart/$', dash_chart),
-    url(r'^(?P<id>\d+)$', dash_detail, name='detail'),
+    # url(r'^(?P<id>\d+)$', dash_detail, name='detail'),
     # url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
     # url(r'^(?P<id>\d+)/delete/$', post_delete),
     #url(r'^posts/$', "<appname>.views.<function_name>"),
--- src/dash/views.py
+++ src/dash/views.py
@@ -1,6 +1,11 @@
+from __future__ import unicode_literals
 import csv
+
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+
 
 # from .forms import DashForm
 from .models import Dash
@@ -14,61 +19,35 @@ def dash_create(request):
         instance.save(using='dashdb')
         reader = csv.reader(request.FILES['file_upload'])
         data = csv.DictReader(request.FILES['file_upload'])
-        print data
     context = {
         'form': form,
      }
     return render(request, 'dash_form.html', context)
 
-    # return HttpResponse('<h1>DASH</h1>')
-
-def dash_detail(request, id=None): #retrieve
-# 	data_instance = get_object_or_404(Dash, id=id)
-# 	context = {
-# 		"title": data_instance.title,
-# 		"data_instance": data_instance,
-# 	}
-# 	return render(request, 'dash_detail.html', context)
-#
-    return HttpResponse('<h1>Dash Detail</h1>')
 def dash_list(request): #list items
-# 	queryset_list = Dash.objects.all().order_by('-timestamp')
-# 	paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
-# 	page_request_var = 'page'
-#
-# 	page = request.GET.get(page_request_var)
-# 	try:
-# 		queryset = paginator.page(page)
-# 	except PageNotAnInteger:
-# 	    # If page is not an integer, deliver first page.
-# 		queryset = paginator.page(1)
-# 	except EmptyPage:
-# 	    # If page is out of range (e.g. 9999), deliver last page of results.
-# 		queryset = paginator.page(paginator.num_pages)
-#
-# 	context = {
-# 		"object_list": queryset,
-# 		"title": "Recent Data",
-# 		'page_request_var': page_request_var
-# 	}
-# 	return render(request, 'dash_list.html', context)
-    return HttpResponse('<h1>Dash List</h1>')
+    context = {
+
+    }
+    return render(request, 'dash_list.html', context)
 
 def dash_chart(request):
-    # xdata = ["Apple", "Apricot", "Avocado", "Banana", "Boysenberries", "Blueberries", "Dates", "Grapefruit", "Kiwi", "Lemon"]
-    # ydata = [52, 48, 160, 94, 75, 71, 490, 82, 46, 17]
-    # chartdata = {'x': xdata, 'y': ydata}
-    # charttype = "pieChart"
-    # chartcontainer = 'piechart_container'
-    data = {
-        # 'charttype': charttype,
-        # 'chartdata': chartdata,
-        # 'chartcontainer': chartcontainer,
-        'extra': {
-            'x_is_date': False,
-            'x_axis_format': '',
-            'tag_script_js': True,
-            'jquery_on_ready': False,
-        }
+    queryset_array  = []
+    for i in Dash.objects.all():
+        queryset_array.append(ContentFile(i.file))
+
+    raw_content_array = []
+    for i in queryset_array:
+        raw_content_array.append(i.read())
+    # print queryset_array
+
+    queryset = Dash.objects.all()
+    # queryset = queryset_array
+
+    context = {
+        'object_list': queryset,
+        'file': 'List',
+        'raw_file': queryset_array,
+        'file_string': raw_content_array,
     }
-    return render(request, 'dash_chart.html', data)
+    # print queryset
+    return render(request, 'dash_chart.html', context)
