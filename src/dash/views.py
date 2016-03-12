dbc0f7619e67950b03c96d581bc82e311abf416e
saposki <saposki@gmail.com>
2016-03-11 17:50:37 -0800

    implement JSON input as data set

--- src/dash/views.py
+++ src/dash/views.py
@@ -38,23 +38,56 @@ def dash_chart(request):
     raw_content_array = []
     for i in queryset_array:
         raw_content_array.append(i.read())
+
+
     # print queryset_array
+
 
     data_string_array = []
     for i in raw_content_array:
-        data_string_array.append(default_storage.open(i).read())
-    # print data_string_array
 
+        data_string_array.append(default_storage.open(i).read())
+
+    # print len(data_string_array)
+    # print data_string_array
+    # print len(data_string_array[0])
+    # print data_string_array[0][0]
+    # print data_string_array[0][1]
+
+    # row = []
+    # row = data_string_array[0][4:len(data_string_array[0])]
+    # print len(row)
+
+
+    # new_row = []
+    # print len(new_row)
+
+    # count = 0
+    # for i in row:
+    #     new_row.append(row.split("\n"))
+    #     count += 1
+    # print len(new_row)
+
+
+    # print data_string_array[0][4]
+    # row_array = []
+
+    # for i in data_string_array:
+        # row_array.append(i.split(','))
+
+    # row_array = row_array[2:len(row_array)]
+    # print len(row_array)
 
     queryset = Dash.objects.all()
     # queryset = queryset
 
     context = {
         'object_list': queryset,
-        'file': 'List',
+        # 'file': 'List',
         'raw_file': queryset_array,
         'file_location': raw_content_array,
         'data_string': data_string_array,
+        # 'row': row_array,
     }
     # print queryset
     return render(request, 'dash_chart.html', context)
