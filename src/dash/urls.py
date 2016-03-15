8e613cfc33ed7f8a9e65c5da2f354496fd627365
saposki <saposki@gmail.com>
2016-03-14 21:12:59 -0700

    created JSON endpoint for D3 argumnent

--- src/dash/urls.py
+++ src/dash/urls.py
@@ -5,6 +5,7 @@ from .views import (
 	dash_list,
 	dash_create,
 	# dash_detail,
+	dash_json_end_point,
 	dash_chart,
 	# post_update,
 	# post_delete,
@@ -13,6 +14,7 @@ from .views import (
 urlpatterns = [
 	url(r'^$', dash_list, name='list'),
     url(r'^create/$', dash_create),
+    url(r'^endpoint/$', dash_json_end_point),
     url(r'^chart/$', dash_chart),
     # url(r'^(?P<id>\d+)$', dash_detail, name='detail'),
     # url(r'^(?P<id>\d+)/edit/$', post_update, name='update'),
--- src/dash/views.py
+++ src/dash/views.py
@@ -1,4 +1,5 @@
 from __future__ import unicode_literals
+from django.http import JsonResponse
 import csv
 import httplib
 import xml.etree.ElementTree as ElementTree
@@ -33,6 +34,33 @@ def dash_list(request): #list items
     }
     return render(request, 'dash_list.html', context)
 
+# end point url D3
+def dash_json_end_point(request):
+
+    queryset_array  = []
+    for i in Dash.objects.all():
+        queryset_array.append(ContentFile(i.file))
+
+    raw_content_array = []
+    for i in queryset_array:
+        raw_content_array.append(i.read())
+
+    data_string_array = []
+    for i in raw_content_array:
+        data_string_array.append(default_storage.open(i).read())
+
+    str = ''
+    for i in data_string_array:
+        str += i
+
+    str_dict = json.loads(str)
+
+    response = JsonResponse(str_dict, safe=False)
+    print type(response)
+
+    return response
+
+
 def dash_chart(request):
     queryset_array  = []
     for i in Dash.objects.all():
@@ -53,6 +81,8 @@ def dash_chart(request):
 # it will need to be converted to a string to use the json.loads()
 # json.loads convert strings to dictonary
 # with correct formatting of the string we should get desired dict.
+# https://docs.python.org/3/library/json.html#json.loads
+# https://docs.python.org/3/library/json.html#json.load
 
 # below we have created a string variable 'str'
 # 'str' will hold the string equivalent of the xml file being read
@@ -66,21 +96,11 @@ def dash_chart(request):
 # 'json.loads() converts 'str' to a valid JSON dict'
 
     str_dict = json.loads(str)
-    print str_dict
+    # print str_dict
 
-
-
-    # xml_container = []
-    # for i in data_string_array:
-        # print i
-
-    # print xml_container
-
-    # print len(data_string_array)
-    # print data_string_array
-    # print len(data_string_array[0])
-    # print data_string_array[0][0]
-    # print data_string_array[0][1]
+#
+    response = JsonResponse(str_dict, safe=False)
+    print type(response)
 
     # row = []
     # row = data_string_array[0][4:len(data_string_array[0])]
@@ -107,7 +127,6 @@ def dash_chart(request):
     # print len(row_array)
 
     queryset = Dash.objects.all()
-    # queryset = queryset
 
     context = {
         'object_list': queryset,
@@ -117,10 +136,7 @@ def dash_chart(request):
         'data_string': data_string_array,
         'json_dict': str_dict,
         # 'row': row_array,
+        'json_end_point': response,
     }
-    # print queryset
-    # {% for i in data_string %}
-    #    {{i}}
-    # {% endfor %}
 
     return render(request, 'dash_chart.html', context)
